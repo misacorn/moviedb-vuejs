100fb3411c6b8af11d5ba7c80a6af78d7194b54b
Misa <dieuhuongngo@gmail.com>
2019-07-14 20:10:55 +0300

    props in component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :title="title" />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -12,6 +12,9 @@ import Header from "./components/Header.vue";
 
 export default {
   name: "app",
+  data() {
+    return { title: "Vue Movie DB" };
+  },
   components: {
     Header,
     HelloWorld
