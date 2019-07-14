d204f968fdd52bd6b9cf8006d7ccc8650dd88a07
Misa <dieuhuongngo@gmail.com>
2019-07-14 20:50:01 +0300

    looping in vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <Header :title="title" />
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <li :key="index" v-for="(item, index) in cats">{{item}}</li>
+      <li :key="person.id" v-for="person in human">{{person.name}}</li>
+      <li :key="index" v-for="(value, key, index) in home">{{key}}: {{value}}</li>
+    </ul>
+    <HelloWorld :key="person.id" v-for="person in human" msg="Welcome to Your Vue.js App" />
+    <!-- this is going to render the components twice -->
   </div>
 </template>
 
@@ -13,7 +19,22 @@ import Header from "./components/Header.vue";
 export default {
   name: "app",
   data() {
-    return { title: "Vue Movie DB" };
+    return {
+      title: "Vue Movie DB",
+      cats: ["Salsa", "Mona", "Titi"],
+      human: [
+        {
+          name: "Misa",
+          id: "jksse"
+        },
+        { name: "Tien", id: "kjserk" }
+      ],
+      home: {
+        add: "Antti Korpin tie 4C",
+        location: "Koskela",
+        city: "Helsinki"
+      }
+    };
   },
   components: {
     Header,
