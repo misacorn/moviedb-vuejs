f12092bebc9ba937520ac1f47204b77b09ac7da2
Misa <dieuhuongngo@gmail.com>
2019-07-16 22:40:22 +0300

    fetching the popurlar movie list

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>{{movie.original_title}}</div>
+  <div>{{movies[0].original_title}}</div>
 </template>
 
 <script>
 export default {
   name: "MovieList",
   data() {
-    return { movie: {} };
+    return { movies: [] };
   },
   created: function() {
     this.fetchData();
@@ -15,10 +15,10 @@ export default {
     fetchData: async function() {
       try {
         const res = await fetch(
-          "https://api.themoviedb.org/3/movie/623?api_key=bf2e9e1f41e52b73e962e7a4c7d1e37b"
+          "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bf2e9e1f41e52b73e962e7a4c7d1e37b"
         );
-        const movie = await res.json();
-        this.movie = movie;
+        const movies = await res.json();
+        this.movies = movies.results;
       } catch (e) {
         console.log(e);
       }
