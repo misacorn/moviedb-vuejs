590fba049a15374f339944fc2bcd0da69c6cbd05
Misa <dieuhuongngo@gmail.com>
2019-07-17 21:14:11 +0300

    array of movie posters

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <img :src="abc" :alt="movie.title" />
+    <img :src="posterImg" :alt="movie.title" />
   </div>
 </template>
 
 <script>
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
+
 export default {
   props: ["movie"],
-  name: "Movie"
+  name: "Movie",
+  computed: {
+    posterImg: function() {
+      return `${POSTER_PATH}/${this.movie.poster_path}`;
+    }
+  }
 };
 </script>
 
