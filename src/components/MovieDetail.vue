83509c29eb52f7c2424e39bad8023f0475625266
Misa <dieuhuongngo@gmail.com>
2019-07-18 21:11:39 +0300

    fetching movie details

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>{{$route.params.id}}</div>
+  <div>{{movie.original_title}}</div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { movie: {} };
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=bf2e9e1f41e52b73e962e7a4c7d1e37b`
+        );
+        const movie = await res.json();
+        this.movie = movie;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
