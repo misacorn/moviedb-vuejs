8857b3ee34bfc4ba0da3068df28f846ccb942593
Misa <dieuhuongngo@gmail.com>
2019-07-21 12:50:13 +0300

    tested transition

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,9 @@
     <h1>
       <router-link to="/">{{title}}</router-link>
     </h1>
-    <h1 v-if="show">Animated</h1>
+    <transition name="fade">
+      <h1 v-if="show">Animated</h1>
+    </transition>
     <button @click="show = !show">Show/Hide</button>
   </header>
 </template>
@@ -27,5 +29,14 @@ header {
 }
 h1 {
   margin: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: scale(0);
 }
 </style>
